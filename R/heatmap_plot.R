58f594f870a7fe80f1f481db6e605a17a1971bb7
Juan Pablo Carreon Hidalgo <jpch_26@outlook.com>
2021-09-23 12:03:09 -0500

    Editions on helpers to set side colors in the heatmap

--- R/heatmap_plot.R
+++ R/heatmap_plot.R
@@ -10,7 +10,7 @@ heatmap_row_names <- function(select, data_set) {
   }
 }
 
-# Helper to put or remove data column names in the heatmaps
+# Helper to put or remove data column names in the heatmap
 heatmap_col_names <- function(select, data_set) {
   if (select == "Put") {
     col_names <- colnames(data_set)
@@ -19,20 +19,21 @@ heatmap_col_names <- function(select, data_set) {
   }
 }
 
-# Helper to set column side colors in the heatmap
-colside_colors <- function(dataset) {
-  column_levels <- as.numeric(as.factor(colnames(dataset)))
-  palette(
-    RColorBrewer::brewer.pal(8, "Dark2")
-    )[column_levels]
-}
-
-# Helper to set row side colors in the heatmap
-rowside_colors <- function(dataset) {
-  row_levels <- as.numeric(as.factor(rownames(dataset)))
-  palette(
-    RColorBrewer::brewer.pal(8, "Dark2")
-    )[row_levels]
+# Helepr to set column or row side colors in the heatmap
+side_colors <- function(dataset, margin = 1) {
+  
+  if (margin == 1) levels_name <- rownames(dataset)
+  else if (margin == 2) levels_name <- colnames(dataset)
+  else stop("Please, choose a valid margin (1 or 2)")
+  
+  levels_name <- as.numeric(as.factor(levels_name))
+  
+  color_ramp <- colorRampPalette(
+    RColorBrewer::brewer.pal(12, "Paired"), bias = 5
+  )(length(levels_name))
+  
+  palette(color_ramp)[levels_name]
+  
 }
 
 # Heatmap plot function ---------------------------------------------------
@@ -41,7 +42,7 @@ heatmap_plot <- function(data_set, heatmap_colors, colside, dendrogram, x_lab,
                          y_lab, xlab_space, ylab_space, row_names, col_names,
                          cex_row, cex_col) {
   ## Heatmap colors
-  plot_colors <-colorRampPalette(
+  plot_colors <- colorRampPalette(
     RColorBrewer::brewer.pal(8, heatmap_colors)
     )(100)
   
@@ -52,7 +53,7 @@ heatmap_plot <- function(data_set, heatmap_colors, colside, dendrogram, x_lab,
       data_set,
       trace = "none",
       dendrogram = dendrogram,
-      ColSideColors = colside_colors(data_set),
+      ColSideColors = side_colors(data_set, 2),
       col = plot_colors,
       labRow = row_names,
       labCol = col_names,
@@ -69,7 +70,7 @@ heatmap_plot <- function(data_set, heatmap_colors, colside, dendrogram, x_lab,
       data_set,
       trace = "none",
       dendrogram = dendrogram,
-      RowSideColors = rowside_colors(data_set),
+      RowSideColors = side_colors(data_set, 1),
       col = plot_colors,
       labRow = row_names,
       labCol = col_names,
@@ -86,8 +87,8 @@ heatmap_plot <- function(data_set, heatmap_colors, colside, dendrogram, x_lab,
       data_set,
       trace = "none",
       dendrogram = dendrogram,
-      RowSideColors = rowside_colors(data_set),
-      ColSideColors = colside_colors(data_set),
+      RowSideColors = side_colors(data_set, 1),
+      ColSideColors = side_colors(data_set, 2),
       col = plot_colors,
       labRow = row_names,
       labCol = col_names,
